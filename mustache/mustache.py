86c742b69392fffbc03e6e0a0f5d7ed9a4b6ec27
Abbas Roayaei Ardakany <abbas@herman-login1.hpc.lji.org>
2020-08-18 09:49:14 -0700

    commented memory_usage

--- mustache/mustache.py
+++ mustache/mustache.py
@@ -1167,7 +1167,7 @@ def main():
 
     
 if __name__ == '__main__':
-    #main()
-    mem_usage = memory_usage(main)
+    main()
+    #mem_usage = memory_usage(main)
     #print('Memory usage (in chunks of .1 seconds): %s' % mem_usage)
-    print('Maximum memory usage: %s' % max(mem_usage))
+    #print('Maximum memory usage: %s' % max(mem_usage))
